4d0539a243def7b540f587582e916daa355153dc
brenley-woodard <brenleyew@gmail.com>
2022-09-22 09:58:54 -0700

    added HTML for search bar, filter, and button

---
+++ fetch-utils.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// export async functions that fetch data
